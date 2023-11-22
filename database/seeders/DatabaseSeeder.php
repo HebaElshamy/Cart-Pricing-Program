330338042897864d59673a9c38e2440c45a453bf
Heba Elshamy <46092804+HebaElshamy@users.noreply.github.com>
2023-11-22 13:09:04 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UsersTableSeeder::class)
         $this->call(CountriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
   
